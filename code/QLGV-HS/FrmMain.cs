627fb872aaf5e398b417d99eef66e7586f6014d9
hungdo2611 <tobmoki@gmail.com>
2018-06-11 10:53:32 +0700

    HUngDO
    This reverts commit 328246c0b859f3f086d766c6629c183412781e3d.

--- code/QLGV-HS/FrmMain.cs
+++ code/QLGV-HS/FrmMain.cs
@@ -29,5 +29,51 @@ namespace QLGV_HS
             frmGiaoVien GV = new frmGiaoVien();
             GV.Show();
         }
+
+        private void thôngTinGiảngDạyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QuanLiGiangDay gd = new QuanLiGiangDay();
+            gd.Show();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xemDanhSáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QuanLyHocSinh hs = new QuanLyHocSinh();
+            hs.Show();
+        }
+
+        private void danhSáchGiáoViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmGiaoVien GV = new frmGiaoVien();
+            GV.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tìmKIếmHọcSinhToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TimKiemHocSInh tk = new TimKiemHocSInh();
+            tk.Show();
+
+        }
+
+        private void timKiếmGiáoViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TimKiemGiaoVien tkgv = new TimKiemGiaoVien();
+            tkgv.Show();
+        }
     }
 }
